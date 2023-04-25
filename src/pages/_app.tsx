d25ee4481c6a6165caca8d5a8cc81f41d6a4d86e
MichalSalek <contact@michalsalek.pl>
2023-04-25 13:43:55 +0200

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import localFont         from 'next/font/local'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+
+const font = localFont({src: [
+    {
+      path: '../../public/fonts/Mona-Sans-Light.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/Mona-Sans-Medium.woff2',
+      weight: '700',
+      style: 'normal',
+    }
+  ]})
+
+export default function App({Component, pageProps}: AppProps) {
+    return <main className={font.className}>
+            <Component {...pageProps} />
+        </main>
 }
