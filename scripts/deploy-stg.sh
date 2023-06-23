ef43c363a812b910e93016f6cec3c1e8b804b7fc
Michał Sałek <contact@michalsalek.pl>
2023-06-24 01:06:10 +0200

    deploy script

--- scripts/deploy-stg.sh
+++ scripts/deploy-stg.sh
@@ -4,3 +4,13 @@ yarn build
 git add out
 git commit -m "Deploy"
 git push origin main
+
+# Server side
+ssh pistachio@s26.mydevil.net  << EOF
+pwd
+cd ./repositories/
+pwd
+bash ./deploy-stg-michalsalek_pl_2023.sh
+echo 'End of deploy script, exiting from ssh.'
+exit
+EOF
