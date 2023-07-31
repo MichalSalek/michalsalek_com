8b322e04c4012248f0776ca3d0ec5495c2d60cf9
Michał Sałek <contact@michalsalek.pl>
2023-07-31 20:48:56 +0200

    Deploy

--- out/_next/static/tBMmvgC0gafiB_JkHMRr4/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcontact","\u002Fsoftware","\u002Fsoftware\u002Farchitecture","\u002Fsoftware\u002Fbackend","\u002Fsoftware\u002Ffrontend","\u002Fsound","\u002Fsound\u002Fsoundtracks","\u002Fsound\u002Fstudio"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
