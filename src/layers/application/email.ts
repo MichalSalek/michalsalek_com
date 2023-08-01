aa48f8050f403479c061447022b8bc48412efe38
Michał Sałek <contact@michalsalek.pl>
2023-08-02 01:24:07 +0200

    simply contact form works with mailer microservice

---
+++ src/layers/application/email.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+
+
+
+type SendEmail = {
+    subject: string
+    text: string
+}
+
+export const sendEmail = (payload: SendEmail) => {
+    if (!process.env.NEXT_PUBLIC_MAILING_SERVICE_URL) {
+        console.warn('No email provider.')
+        return void undefined
+    }
+
+    axios.post(process.env.NEXT_PUBLIC_MAILING_SERVICE_URL, payload)
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,8 @@
+import { sendEmail }                        from '@/src/layers/application/email'
 import { GeneralPageProps }                 from '@/src/pages/_app'
 import { Container, TextField, Typography } from '@mui/material'
+import Button                               from '@mui/material/Button'
+import { useCallback, useState }            from 'react'
 
 
 
@@ -7,11 +10,31 @@ import { Container, TextField, Typography } from '@mui/material'
 const PAGE_TITLE = 'Contact with the office'
 
 export default function Page() {
+
+    const [subject, setSubject] = useState('')
+    const [text, setText] = useState('')
+
+    const sendEmailCallback = useCallback(() => {
+        sendEmail({subject, text})
+    }, [subject, text])
+
     return <Container>
 
         <Typography variant={'h1'}>{PAGE_TITLE}</Typography>
 
-        <TextField variant="outlined" placeholder={'Contact form'}/>
+        <TextField
+            variant="outlined"
+                   placeholder={'Subject'}
+                   onBlur={(event) => setSubject(event.target.value)}
+        />
+        <TextField
+            multiline={true}
+            variant="outlined"
+            placeholder={'Text'}
+            onBlur={(event) => setText(event.target.value)}
+        />
+
+        <Button onClick={sendEmailCallback}>send</Button>
 
     </Container>
 }
