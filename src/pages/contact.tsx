21da0eb9f929c52c141d8fcb4f768c83133b83e5
Michał Sałek <contact@michalsalek.pl>
2023-08-03 22:42:09 +0200

    simple contact form logic flow

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,9 @@
-import { sendEmail }                        from '@/src/layers/application/email'
-import { GeneralPageProps }                 from '@/src/pages/_app'
-import { Container, TextField, Typography } from '@mui/material'
-import Button                               from '@mui/material/Button'
-import { useCallback, useState }            from 'react'
+import { sendEmail }                                     from '@/src/layers/application/email'
+import { GeneralPageProps }                              from '@/src/pages/_app'
+import { freezeThreadAndWait }                           from '@msalek/utils'
+import { Container, FormControl, TextField, Typography } from '@mui/material'
+import Button                                            from '@mui/material/Button'
+import { useState }                                      from 'react'
 
 
 
@@ -14,27 +15,74 @@ export default function Page() {
     const [subject, setSubject] = useState('')
     const [text, setText] = useState('')
 
-    const sendEmailCallback = useCallback(() => {
-        sendEmail({subject, text})
-    }, [subject, text])
+    const clearForm = () => {
+        setSubject('')
+        setText('')
+    }
+
+
+    const [formDisabled, setFormDisabled] = useState(false)
+    const [formError, setFormError] = useState(false)
+
+
+    const sendEmailCallback = async () => {
+        setFormDisabled(true)
+
+        if (subject && text) {
+            await sendEmail({subject, text})
+            await freezeThreadAndWait(500)
+            clearForm()
+
+        } else {
+            setFormDisabled(false)
+            setFormError(true)
+            await freezeThreadAndWait(400)
+            setFormError(false)
+            await freezeThreadAndWait(200)
+            setFormError(true)
+            await freezeThreadAndWait(300)
+            setFormError(false)
+        }
+
+        await freezeThreadAndWait(300)
+        setFormDisabled(false)
+    }
 
     return <Container>
 
         <Typography variant={'h1'}>{PAGE_TITLE}</Typography>
 
-        <TextField
-            variant="outlined"
-                   placeholder={'Subject'}
-                   onBlur={(event) => setSubject(event.target.value)}
-        />
-        <TextField
-            multiline={true}
-            variant="outlined"
-            placeholder={'Text'}
-            onBlur={(event) => setText(event.target.value)}
-        />
+        <FormControl
+            component={'section'}
+        >
 
-        <Button onClick={sendEmailCallback}>send</Button>
+            <TextField
+                error={formError}
+                disabled={formDisabled}
+                placeholder={'Subject'}
+                value={subject}
+                onChange={(event) =>
+                    setSubject(event.target.value)}
+            />
+            <TextField
+                error={formError}
+                disabled={formDisabled}
+                multiline={true}
+                placeholder={'Text'}
+                value={text}
+                onChange={(event) =>
+                    setText(event.target.value)}
+            />
+
+            <Button
+                color={formError ? 'error' : 'primary'}
+                disabled={formDisabled}
+                onClick={sendEmailCallback}
+                sx={{my: 5}}>send</Button>
+
+        </FormControl>
+
+
 
     </Container>
 }
