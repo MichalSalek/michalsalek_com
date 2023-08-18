ae9593819983a481613a236a118da4b0fadb3a12
Michał Sałek <contact@michalsalek.pl>
2023-08-18 16:53:56 +0200

    Deploy

--- src/layers/application/GA.controller.tsx
+++ src/layers/application/GA.controller.tsx
@@ -1,5 +1,5 @@
-import { isProductionEnv }                           from '@msalek/utils'
-import { ReactElement, useEffect, useRef, useState } from 'react'
+import { isProductionEnv }   from '@msalek/utils'
+import { useEffect, useRef } from 'react'
 
 
 
@@ -21,14 +21,13 @@ export const GAController = (): null => {
         const script2 = document.createElement('script')
         script2.innerHTML = 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'G-KTZFFWKZ41\');'
 
+        window?.addEventListener('load', () => {
+            setTimeout(() => {
+                __next?.append(script1)
+                __next?.append(script2)
+            }, 1000)
+        })
 
-        setTimeout(() => {
-
-            __next?.append(script1)
-            __next?.append(script2)
-
-
-        }, 1500)
 
     }, [])
 
