fa5b6cbfbca528658198848ba98e8407019a7e21
Michał Sałek <contact@michalsalek.pl>
2023-08-18 18:56:00 +0200

    GA loading by interaction

--- src/layers/application/GA.controller.tsx
+++ src/layers/application/GA.controller.tsx
@@ -21,12 +21,21 @@ export const GAController = (): null => {
         const script2 = document.createElement('script')
         script2.innerHTML = 'window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag(\'js\', new Date()); gtag(\'config\', \'G-KTZFFWKZ41\');'
 
-        window?.addEventListener('load', () => {
+
+
+        const userEvents = ['scroll', 'keydown', 'pointerdown', 'pointermove', 'touchstart']
+        let runOnce2 = false
+        const callbackClosure = () => {
+            if (runOnce2) return void undefined
+            runOnce2 = true
             setTimeout(() => {
                 __next?.append(script1)
                 __next?.append(script2)
             }, 1000)
-        })
+            userEvents.forEach((eventName) => document?.removeEventListener(eventName, callbackClosure))
+        }
+
+        userEvents.forEach((eventName) => document?.addEventListener(eventName, callbackClosure))
 
 
     }, [])
