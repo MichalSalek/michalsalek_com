2e8d5231c4a39706ce6fc0f7f9e7a8185fc2be01
Michał Sałek <contact@michalsalek.pl>
2023-08-25 14:30:35 +0200

    ssr on - CLS incrase

--- src/UI/components/_lazyComponents.aggregate.ts
+++ src/UI/components/_lazyComponents.aggregate.ts
@@ -5,20 +5,20 @@ import dynamic from 'next/dynamic'
 
 export const InfoTextBlockMoleculeLazy =
     dynamic(() => import('./InfoTextBlock.molecule'), {
-        ssr: false
+        ssr: true
     })
 
 export const RootCardOrganismLazy =
     dynamic(() => import('./RootCard.organism'), {
-        ssr: false
+        ssr: true
     })
 
 export const ContentBlockOrganismLazy =
     dynamic(() => import('./ContentBlock.organism'), {
-        ssr: false
+        ssr: true
     })
 
 export const InternalLinkingMoleculeLazy =
     dynamic(() => import('./InternalLinking.molecule'), {
-        ssr: false
+        ssr: true
     })
