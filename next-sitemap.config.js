9a853cd89e445f9cb1510f4d7ceda6f25e388ffb
Michał Sałek <contact@michalsalek.pl>
2023-09-27 21:31:52 +0200

    sitemap env var rename

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 // https://www.npmjs.com/package/next-sitemap
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL, generateRobotsTxt: true, output: 'export' // Must be equal with output option in next.config
+    siteUrl: process.env.SITEMAP_SITE_URL, generateRobotsTxt: true, output: 'export' // Must be equal with output option in next.config
 }
